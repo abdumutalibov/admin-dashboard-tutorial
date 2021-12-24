627b36e7820e77d9b02602a6fdd1bccf3aef74aa
abdumutalibov <azimabdumutalibov21@gmail.com>
2021-12-24 23:51:45 +0500

    user

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import './app.css'
+import "./app.css";
 import Home from "./pages/home/Home";
-import {BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 function App() {
   return (
     <Router>
-
-   <Topbar/>
-   <div className="container">
-     <Sidebar/>
-     <Routes>
-<Route path='/' element={<Home/>}/>
-<Route path='/users' element={<UserList/>}/>
-     </Routes>
-   </div>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/dummyData.js
+++ src/dummyData.js
@@ -49,3 +49,96 @@ export const userData = [
       },
   
   ];
+
+ export const userRows = [
+    {
+      id: 1,
+      username: "Jon Snow",
+      avatar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3ceMuX4H0-RcBT3BiL4dndVvRYK98Gb-1ryc6ISnWB2zcq6lkJt3lgXCYIX9KvptzeU&usqp=CAU",
+      email: "hon@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 2,
+      username: "Jon Snow",
+      avatar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3ceMuX4H0-RcBT3BiL4dndVvRYK98Gb-1ryc6ISnWB2zcq6lkJt3lgXCYIX9KvptzeU&usqp=CAU",
+      email: "hon@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 3,
+      username: "Jon Snow",
+      avatar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3ceMuX4H0-RcBT3BiL4dndVvRYK98Gb-1ryc6ISnWB2zcq6lkJt3lgXCYIX9KvptzeU&usqp=CAU",
+      email: "hon@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 4,
+      username: "Jon Snow",
+      avatar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3ceMuX4H0-RcBT3BiL4dndVvRYK98Gb-1ryc6ISnWB2zcq6lkJt3lgXCYIX9KvptzeU&usqp=CAU",
+      email: "hon@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 5,
+      username: "Jon Snow",
+      avatar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3ceMuX4H0-RcBT3BiL4dndVvRYK98Gb-1ryc6ISnWB2zcq6lkJt3lgXCYIX9KvptzeU&usqp=CAU",
+      email: "hon@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 6,
+      username: "Jon Snow",
+      avatar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3ceMuX4H0-RcBT3BiL4dndVvRYK98Gb-1ryc6ISnWB2zcq6lkJt3lgXCYIX9KvptzeU&usqp=CAU",
+      email: "hon@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 7,
+      username: "Jon Snow",
+      avatar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3ceMuX4H0-RcBT3BiL4dndVvRYK98Gb-1ryc6ISnWB2zcq6lkJt3lgXCYIX9KvptzeU&usqp=CAU",
+      email: "hon@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 8,
+      username: "Jon Snow",
+      avatar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3ceMuX4H0-RcBT3BiL4dndVvRYK98Gb-1ryc6ISnWB2zcq6lkJt3lgXCYIX9KvptzeU&usqp=CAU",
+      email: "hon@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 9,
+      username: "Jon Snow",
+      avatar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3ceMuX4H0-RcBT3BiL4dndVvRYK98Gb-1ryc6ISnWB2zcq6lkJt3lgXCYIX9KvptzeU&usqp=CAU",
+      email: "hon@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 10,
+      username: "Jon Snow",
+      avatar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3ceMuX4H0-RcBT3BiL4dndVvRYK98Gb-1ryc6ISnWB2zcq6lkJt3lgXCYIX9KvptzeU&usqp=CAU",
+      email: "hon@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+  ];
